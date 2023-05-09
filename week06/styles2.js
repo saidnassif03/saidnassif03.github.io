5a24ef39c70c3a5ca974ee2d239c2827a3f5235e
saidnassif03 <124061104+saidnassif03@users.noreply.github.com>
2023-05-08 21:07:52 -0400

    Add files via upload

---
+++ week06/styles2.js
@@ -0,0 +1,37 @@
+let moons = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+}
+
+function draw() {
+    for (let moon of moons) {
+        moon.display();
+        moon.update();
+    }
+}
+
+function mouseMoved() {
+    moons.push(new Moon(mouseX, mouseY));
+}
+
+class Moon {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = random(10, 50); // Increased minimum and maximum size
+        this.color = color(random(100, 255), random(100, 255), random(100, 255));
+    }
+
+    display() {
+        noStroke();
+        fill(this.color);
+        ellipse(this.x, this.y, this.size);
+    }
+
+    update() {
+        this.x += random(-1, 1);
+        this.y += random(-1, 1);
+    }
+}
